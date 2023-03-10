00154a824105108c9276f6423f0a0c6ea697268c
cecilcodespython <cecilselorm34@gmaill.com>
2023-03-10 12:28:47 +0000

    slices

---
+++ index.js
@@ -0,0 +1,16 @@
+let Name = "Selorm Cecil";
+
+firstName = Name.slice(0,6);
+
+lastName = Name.slice(6)
+
+
+console.log(firstName,lastName)
+
+
+fName = Name.slice(0,Name.indexOf(" "));
+
+lName = Name.slice(Name.indexOf(" "))
+
+
+console.log(fName,lName)
